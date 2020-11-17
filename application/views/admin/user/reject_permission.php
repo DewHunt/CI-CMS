6bd70194c7c87fe6c2b2b413a976f93f3f6d8493
DewHunt <dew.fog1553@gmail.com>
2020-11-18 03:28:35 +0600

    Update - 20201118

--- application/views/admin/user/reject_permission.php
+++ application/views/admin/user/reject_permission.php
@@ -23,131 +23,145 @@
 
    	<?php if ($rootMenu->parent_menu == NULL): ?>
         <?php if ($countUserMenuAction->count == 0): ?>
-        	<?php
-        		$parentMenus = $this->Helper_model->get_all_menus($rootMenu->id);
-        	?>
+        	<?php $parentMenus = $this->Helper_model->get_all_menus($rootMenu->id); ?>
 
-            <div class="row parentMenuBlock">
-                <div class="col-md-12">
-                	<input class="parentMenu_<?= $rootMenu->parent_menu ?> menu" type="checkbox" name="usermenu[]" value="<?= $rootMenu->id ?>" <?= $checked ?>  data-id="<?= $rootMenu->id ?>" <?php if ($rootMenu->id == 1): ?>onclick="return false" checked<?php endif ?>>
-                    <span><?= $rootMenu->menu_name ?></span>
-                  
-                    <div class="row" style="padding-left: 30px;">
-                        <?php foreach ($parentMenus as $parentMenu): ?>
-                        	<?php
-                                $userMenuAction = $this->Helper_model->get_user_menu_action($parentMenu->id);
-                                $rolePermission = explode(',', $userRoles->permission);
-                                if (in_array($parentMenu->id, $rolePermission))
-                                {
-                                    $checked = "checked";
-                                }
-                                else
-                                {
-                                    $checked = "";
-                                }
-                        	?>
+        	<?php if ($checked == "checked"): ?>
+	            <div class="row parentMenuBlock">
+	                <div class="col-md-12">
+	                	<input class="parentMenu_<?= $rootMenu->parent_menu ?> menu" type="checkbox" name="usermenu[]" value="<?= $rootMenu->id ?>" <?= $checked ?>  data-id="<?= $rootMenu->id ?>" <?php if ($rootMenu->id == 1): ?>onclick="return false" checked<?php endif ?>>
+	                    <span><?= $rootMenu->menu_name ?></span>
+	                  
+	                    <div class="row" style="padding-left: 30px;">
+	                        <?php foreach ($parentMenus as $parentMenu): ?>
+	                        	<?php
+	                                $userMenuAction = $this->Helper_model->get_user_menu_action($parentMenu->id);
+	                                $rolePermission = explode(',', $userRoles->permission);
+	                                if (in_array($parentMenu->id, $rolePermission))
+	                                {
+	                                    $checked = "checked";
+	                                }
+	                                else
+	                                {
+	                                    $checked = "";
+	                                }
+	                        	?>
 
-                            <div class="col-md-3">
-                                <input class="parentMenu_<?= $parentMenu->parent_menu ?> menu" type="checkbox" name="usermenu[]" value="<?= $parentMenu->id ?>" <?= $checked ?>  data-id="<?= $parentMenu->id ?>">
-                                <span><?= $parentMenu->menu_name ?></span>
-                                <div style="margin-left: 26px;margin-top: 3px;">
-                                    <?php foreach ($userMenuAction as $action): ?>
-                                    	<?php
-                                            $actionPermission = explode(',', $userRoles->action_permission);
-                                            if (in_array($action->id, $actionPermission))
-                                            {
-                                                $actionChecked = "checked";
-                                            }
-                                            else
-                                            {
-                                                $actionChecked = "";
-                                            }
-                                    	?>
-                                        <input class="childMenu_<?= $action->parent_menu_id ?>" type="checkbox" name="usermenuAction[]" value="<?= $action->id ?>" style="margin-bottom: 8px;" <?= $actionChecked ?>> <?= $action->action_name ?> <br>
-                                    <?php endforeach ?>
-                                </div>
-                  
-                                <div class="row" style="padding-left: 30px;">
-                                	<?php
-                                		$subMenus = $this->Helper_model->get_all_menus($parentMenu->id);
-                                	?>
-                                    <?php foreach ($subMenus as $subMenu): ?>
-                                    	<?php
-                                            $userMenuAction = $this->Helper_model->get_user_menu_action($subMenu->id);
-                                            $rolePermission = explode(',', $userRoles->permission);
-                                            if (in_array($subMenu->id, $rolePermission))
-                                            {
-                                                $checked = "checked";
-                                            }
-                                            else
-                                            {
-                                                $checked = "";
-                                            }
-                                    	?>
+	                        	<?php if ($checked == "checked"): ?>
+		                            <div class="col-md-3">
+		                                <input class="parentMenu_<?= $parentMenu->parent_menu ?> menu" type="checkbox" name="usermenu[]" value="<?= $parentMenu->id ?>" <?= $checked ?>  data-id="<?= $parentMenu->id ?>">
+		                                <span><?= $parentMenu->menu_name ?></span>
+		                                <div style="margin-left: 26px;margin-top: 3px;">
+		                                    <?php foreach ($userMenuAction as $action): ?>
+		                                    	<?php
+		                                            $actionPermission = explode(',', $userRoles->action_permission);
+		                                            if (in_array($action->id, $actionPermission))
+		                                            {
+		                                                $actionChecked = "checked";
+		                                            }
+		                                            else
+		                                            {
+		                                                $actionChecked = "";
+		                                            }
+		                                    	?>
 
-                                        <div class="col-sm-12">
-                                            <input class="parentMenu_<?= $subMenu->parent_menu ?> menu" type="checkbox" name="usermenu[]" value="<?= $subMenu->id ?>" <?= $checked ?> data-id="<?= $subMenu->id ?>">
-                                            <span><?= $subMenu->menu_name ?></span>
-                                            <div style="margin-left: 26px;margin-top: 3px;">
-                                                <?php foreach ($userMenuAction as $action): ?>
-                                                	<?php
-                                                        $actionPermission = explode(',', $userRoles->action_permission);
-                                                        if (in_array($action->id, $actionPermission))
-                                                        {
-                                                            $actionChecked = "checked";
-                                                        }
-                                                        else
-                                                        {
-                                                            $actionChecked = "";
-                                                        }
-                                                    ?>
-                                                    <input class="childMenu_<?= $action->parent_menu_id ?>" type="checkbox" name="usermenuAction[]" value="<?= $action->id ?>" style="margin-bottom: 8px;" <?= $actionChecked ?>> <?= $action->action_name ?> <br>
-                                                <?php endforeach ?>
-                                            </div>
-                                        </div>
-                                    <?php endforeach ?>
-                                </div>
-                            </div>
-                        <?php endforeach ?>
-                    </div>
-                </div>
-            </div>
+		                                    	<?php if ($actionChecked == "checked"): ?>
+		                                    		<input class="childMenu_<?= $action->parent_menu_id ?>" type="checkbox" name="usermenuAction[]" value="<?= $action->id ?>" style="margin-bottom: 8px;" <?= $actionChecked ?>> <?= $action->action_name ?> <br>
+		                                    	<?php endif ?>		                                        
+		                                    <?php endforeach ?>
+		                                </div>
+		                  
+		                                <div class="row" style="padding-left: 30px;">
+		                                	<?php
+		                                		$subMenus = $this->Helper_model->get_all_menus($parentMenu->id);
+		                                	?>
+		                                    <?php foreach ($subMenus as $subMenu): ?>
+		                                    	<?php
+		                                            $userMenuAction = $this->Helper_model->get_user_menu_action($subMenu->id);
+		                                            $rolePermission = explode(',', $userRoles->permission);
+		                                            if (in_array($subMenu->id, $rolePermission))
+		                                            {
+		                                                $checked = "checked";
+		                                            }
+		                                            else
+		                                            {
+		                                                $checked = "";
+		                                            }
+		                                    	?>
+
+		                                    	<?php if ($checked == "checked"): ?>
+			                                        <div class="col-sm-12">
+			                                            <input class="parentMenu_<?= $subMenu->parent_menu ?> menu" type="checkbox" name="usermenu[]" value="<?= $subMenu->id ?>" <?= $checked ?> data-id="<?= $subMenu->id ?>">
+			                                            <span><?= $subMenu->menu_name ?></span>
+			                                            <div style="margin-left: 26px;margin-top: 3px;">
+			                                                <?php foreach ($userMenuAction as $action): ?>
+			                                                	<?php
+			                                                        $actionPermission = explode(',', $userRoles->action_permission);
+			                                                        if (in_array($action->id, $actionPermission))
+			                                                        {
+			                                                            $actionChecked = "checked";
+			                                                        }
+			                                                        else
+			                                                        {
+			                                                            $actionChecked = "";
+			                                                        }
+			                                                    ?>
+			                                                    <?php if ($actionChecked == "checked"): ?>
+			                                                    	<input class="childMenu_<?= $action->parent_menu_id ?>" type="checkbox" name="usermenuAction[]" value="<?= $action->id ?>" style="margin-bottom: 8px;" <?= $actionChecked ?>> <?= $action->action_name ?> <br>
+			                                                    <?php endif ?>			                                                    
+			                                                <?php endforeach ?>
+			                                            </div>
+			                                        </div>		                                    		
+		                                    	<?php endif ?>
+		                                    <?php endforeach ?>
+		                                </div>
+		                            </div>	                        		
+	                        	<?php endif ?>
+	                        <?php endforeach ?>
+	                    </div>
+	                </div>
+	            </div>        		
+        	<?php endif ?>
         <?php else: ?>
-            <div class="row parentMenuBlock">
-            	<?php
-                    $userMenuAction = $this->Helper_model->get_user_menu_action($rootMenu->id);
-                    $rolePermission = explode(',', $userRoles->permission);
-                    if (in_array($rootMenu->id, $rolePermission))
-                    {
-                        $checked = "checked";
-                    }
-                    else
-                    {
-                        $checked = "";
-                    }
-                ?>
+        	<?php
+                $userMenuAction = $this->Helper_model->get_user_menu_action($rootMenu->id);
+                $rolePermission = explode(',', $userRoles->permission);
+                if (in_array($rootMenu->id, $rolePermission))
+                {
+                    $checked = "checked";
+                }
+                else
+                {
+                    $checked = "";
+                }
+            ?>
+            <?php if ($checked == "checked"): ?>
+	            <div class="row parentMenuBlock">
+	                <div class="col-md-12">
+	                    <input class="parentMenu_<?= $rootMenu->parent_menu ?> menu" type="checkbox" name="usermenu[]" value="<?= $rootMenu->id ?>" <?= $checked ?> data-id="<?= $rootMenu->id ?>">
+	                    <span><?= $rootMenu->menu_name ?></span>
+	                    <div style="margin-left: 26px;margin-top: 3px;">
+	                        <?php foreach ($userMenuAction as $action): ?>
+	                        	<?php
+	                                $actionPermission = explode(',', $userRoles->action_permission);
+	                                if (in_array($action->id, $actionPermission))
+	                                {
+	                                    $actionChecked = "checked";
+	                                }
+	                                else
+	                                {
+	                                    $actionChecked = "";
+	                                }
+								?>
 
-                <div class="col-md-12">
-                    <input class="parentMenu_<?= $rootMenu->parent_menu ?> menu" type="checkbox" name="usermenu[]" value="<?= $rootMenu->id ?>" <?= $checked ?> data-id="<?= $rootMenu->id ?>">
-                    <span><?= $rootMenu->menu_name ?></span>
-                    <div style="margin-left: 26px;margin-top: 3px;">
-                        <?php foreach ($userMenuAction as $action): ?>
-                        	<?php
-                                $actionPermission = explode(',', $userRoles->action_permission);
-                                if (in_array($action->id, $actionPermission))
-                                {
-                                    $actionChecked = "checked";
-                                }
-                                else
-                                {
-                                    $actionChecked = "";
-                                }
-							?>
-                            <input class="childMenu_<?= $action->parent_menu_id ?>" type="checkbox" name="usermenuAction[]" value="<?= $action->id ?>" style="margin-bottom: 8px;" <?= $actionChecked ?>> <?= $action->action_name ?> <br>
-                        <?php endforeach ?>
-                    </div>
-                </div>
-            </div>
+								<?php if ($actionChecked == "checked"): ?>
+									<input class="childMenu_<?= $action->parent_menu_id ?>" type="checkbox" name="usermenuAction[]" value="<?= $action->id ?>" style="margin-bottom: 8px;" <?= $actionChecked ?>> <?= $action->action_name ?> <br>
+								<?php endif ?>
+	                            
+	                        <?php endforeach ?>
+	                    </div>
+	                </div>
+	            </div>
+            <?php endif ?>
         <?php endif ?>
     <?php endif ?>
 <?php endforeach ?>
